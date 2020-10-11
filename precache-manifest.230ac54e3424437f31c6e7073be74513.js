ec6a0ca3b07f3355b2b972a038794455e9fbbb0f
Akash Murthy <akash.360@aol.com>
2020-10-11 02:19:02 +0100

    Updates

---
+++ precache-manifest.230ac54e3424437f31c6e7073be74513.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79f24df70d8a1007ee2dbd83b43a1f29",
+    "url": "/Thrifleganger/cloud-inversion/index.html"
+  },
+  {
+    "revision": "5bf18874650f20d46b13",
+    "url": "/Thrifleganger/cloud-inversion/static/css/2.db125c65.chunk.css"
+  },
+  {
+    "revision": "3631011697968f5c1b48",
+    "url": "/Thrifleganger/cloud-inversion/static/css/main.a55b814a.chunk.css"
+  },
+  {
+    "revision": "5bf18874650f20d46b13",
+    "url": "/Thrifleganger/cloud-inversion/static/js/2.cca33f6d.chunk.js"
+  },
+  {
+    "revision": "07f18e979bdf47624c6d8650b13bd950",
+    "url": "/Thrifleganger/cloud-inversion/static/js/2.cca33f6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3631011697968f5c1b48",
+    "url": "/Thrifleganger/cloud-inversion/static/js/main.0e03d7eb.chunk.js"
+  },
+  {
+    "revision": "c450c7145283850f9247",
+    "url": "/Thrifleganger/cloud-inversion/static/js/runtime-main.c47afcb2.js"
+  }
+]);
