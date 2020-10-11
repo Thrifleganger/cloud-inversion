607e08cc63545f4e0f3ee2ded181417af29a364d
Akash Murthy <akash.360@aol.com>
2020-10-11 02:41:25 +0100

    Updates

---
+++ precache-manifest.aab33e51a4d49df87d6c4d6f2acc422c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9a69aea1d741540e9854a0747001ba8",
+    "url": "/cloud-inversion/index.html"
+  },
+  {
+    "revision": "69dd8991bb211d99298e",
+    "url": "/cloud-inversion/static/css/2.db125c65.chunk.css"
+  },
+  {
+    "revision": "b389945989c41a20747c",
+    "url": "/cloud-inversion/static/css/main.a55b814a.chunk.css"
+  },
+  {
+    "revision": "69dd8991bb211d99298e",
+    "url": "/cloud-inversion/static/js/2.cb4b60cf.chunk.js"
+  },
+  {
+    "revision": "07f18e979bdf47624c6d8650b13bd950",
+    "url": "/cloud-inversion/static/js/2.cb4b60cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b389945989c41a20747c",
+    "url": "/cloud-inversion/static/js/main.3dc19c60.chunk.js"
+  },
+  {
+    "revision": "eec54e8d3be7bdf34fcd",
+    "url": "/cloud-inversion/static/js/runtime-main.75b9123b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Thrifleganger/cloud-inversion/precache-manifest.6f3821c0cfc4ef270a5d3b72bb05b2d1.js"
+  "/cloud-inversion/precache-manifest.aab33e51a4d49df87d6c4d6f2acc422c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Thrifleganger/cloud-inversion/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cloud-inversion/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
